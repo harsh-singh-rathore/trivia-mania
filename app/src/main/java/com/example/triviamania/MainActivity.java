e0de2af3b1a1fb06a90e8c7669c8e63290dc98db
thecreator-hr <harathodhp@gmail.com>
2022-04-05 11:21:38 +0530

    nav bar implemented

--- app/src/main/java/com/example/triviamania/MainActivity.java
+++ app/src/main/java/com/example/triviamania/MainActivity.java
@@ -4,12 +4,17 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -21,22 +26,53 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogOut;
+    FrameLayout frameLayout;
     FirebaseAuth mAuth;
+    BottomNavigationView btmNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogOut = findViewById(R.id.btnLogout);
+
+        frameLayout = findViewById(R.id.frameLayout);
+
+        btnLogOut = findViewById(R.id.btnLogOut);
+        btmNavView = findViewById(R.id.bottomNavView);
         mAuth = FirebaseAuth.getInstance();
+        replaceFragment(new HomeFragment());
 
         btnLogOut.setOnClickListener(view ->{
             mAuth.signOut();
             startActivity(new Intent(MainActivity.this, activity_login.class));
         });
 
+        btmNavView.setOnItemSelectedListener(item -> {
 
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.score:
+                    replaceFragment(new ScoreFragment());
+                    break;
+            }
+
+            return true;
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
